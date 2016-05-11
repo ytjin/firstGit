51903bfed7de414ac914aa4340e8c2e5f551f8a3
ytjin <ytjin@hanbat.ac.kr>
2016-05-11 20:46:20 +0900

    merge conflict test

--- firstGit/src/firstGit/Test1.java
+++ firstGit/src/firstGit/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("Git is fun");
-// make develop branch and test
+// make develop branch and test. merge conflict test
 		System.out.println("Git is much fun");
 	}
 
