764c606562660dcd7a353be818fe44d6dd4dcb9a
ytjin <ytjin@hanbat.ac.kr>
2016-05-11 16:11:42 +0900

    insert additional print code

--- firstGit/src/firstGit/Test1.java
+++ firstGit/src/firstGit/Test1.java
@@ -4,8 +4,8 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("Git is fun");
-
-
+// make develop branch and test
+		System.out.println("Git is much fun");
 	}
 
 }
